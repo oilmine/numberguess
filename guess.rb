7f46c1ea6fa159b4f79475931221f3299a1bf405
Bliss <investmentslips2@gmail.com>
2026-07-25 21:13:26 +0530

    v0.1.0-u2 => Screen clear, text changes, proper code indentation

--- guess.rb
+++ guess.rb
@@ -1,20 +1,22 @@
 def logic(range)
   num = rand(range)
   loop do
-    print "GUESS THE NUMBER -> "
+    print "GUESS THE NUMBER: "
     guess = gets.chomp.to_i
     if guess > range
-      puts "Number is not in range!"
+      puts "Your guess is not in range."
     elsif guess < num
-      puts "Number is too low!"
+      puts "Your guess is too low!"
     elsif guess > num
-      puts "Number is toohi!"
+      puts "Your guess is toohi!"
     elsif guess == num
       puts "You guessed it! The number was #{num}."
       break
     end
   end
 end
+
+system("clear")
 
 EZ_RANGE = 10
 MD_RANGE = 50
@@ -24,24 +26,24 @@ BIN_RANGE = 2
 loop do
   df_lvl = ["easy", "medium", "hard", "binary"]
 
-puts ""
-print "TYPE DIFFICULTY LEVEL -> "
-df = gets.chomp
-df = df.downcase
-puts ""
+  puts ""
+  print "Enter difficulty level (Easy, Medium, Hard, Binary): "
+  df = gets.chomp
+  df = df.downcase
+  puts ""
 
-if !df_lvl.include?(df)
-  puts "Invalid difficulty."
-  exit
-end
+  if !df_lvl.include?(df)
+    puts "Invalid difficulty."
+    exit
+  end
 
-if df == "easy"
-  logic(EZ_RANGE)
-elsif df == "medium"
-  logic(MD_RANGE)
-elsif df == "hard"
-  logic(HD_RANGE)
-elsif df == "binary"
-  logic(BIN_RANGE)
-end
+  if df == "easy"
+    logic(EZ_RANGE)
+  elsif df == "medium"
+    logic(MD_RANGE)
+  elsif df == "hard"
+    logic(HD_RANGE)
+  elsif df == "binary"
+    logic(BIN_RANGE)
+  end
 end
