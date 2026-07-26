9ca24b8c03574136e89704bfd309ba9acedd2850
Bliss <investmentslips2@gmail.com>
2026-07-26 09:21:56 +0530

    I backtrack on my previous commit/statement

--- main.rb
+++ main.rb
@@ -5,89 +5,93 @@ require_relative "src/ui"
 
 title
 
-begin
-  loop do
-  gm_lvl = ["classic", "binary", "hoc"]
-  df_lvl = ["easy", "medium", "hard", "custom"]
+def main()
+  begin
+    loop do
+    gm_lvl = ["classic", "binary", "hoc"]
+    df_lvl = ["easy", "medium", "hard", "custom"]
 
-  puts ""
-  puts "______________________________________"
-  print "|   Classic   |   "
-  print "Binary   |   "
-  print "HoC   |  "
-  puts ""
-  puts ""
-  print "CHOOSE THE GAMEMODE: "
-  gm = gets.chomp
-  gm = gm.downcase
-  puts ""
+    puts ""
+    puts "______________________________________"
+    print "|   Classic   |   "
+    print "Binary   |   "
+    print "HoC   |  "
+    puts ""
+    puts ""
+    print "CHOOSE THE GAMEMODE: "
+    gm = gets.chomp
+    gm = gm.downcase
+    puts ""
 
-  if !gm_lvl.include?(gm)
-    puts "Invalid gamemode."
+    if !gm_lvl.include?(gm)
+      puts "Invalid gamemode."
+      exit
+    end
+
+    if gm == "classic"
+      puts "_________________________________________________"
+      print "|   Easy   |   "
+      print "Medium   |   "
+      print "Hard   |   "
+      print "Custom   |   "
+      puts ""
+      puts ""
+      print "CHOOSE THE DIFFICULTY: "
+      df = gets.chomp
+      df = df.downcase
+      puts ""
+
+      if !df_lvl.include?(df)
+        puts "Invalid difficulty."
+        exit
+      end
+
+      if df == "easy"
+        cl_logic(EASY_RANGE)
+      elsif df == "medium"
+        cl_logic(MID_RANGE)
+      elsif df == "hard"
+        cl_logic(HARD_RANGE)
+      elsif df == "custom"
+        cl_cs_logic
+      end
+    end
+
+    if gm == "binary"
+      bin_logic
+    end
+
+    if gm == "hoc"
+      puts "____________________________________"
+      print "|   Easy   |   "
+      print "Medium   |   "
+      print "Hard   |   "
+      puts ""
+      puts ""
+      print "CHOOSE THE DIFFICULTY: "
+      df = gets.chomp
+      df = df.downcase
+      puts ""
+
+      if !df_lvl.include?(df)
+        puts "Invalid difficulty."
+        exit
+      end
+
+      if df == "easy"
+        hoc_logic(EASY_RANGE)
+      elsif df == "medium"
+        hoc_logic(MID_RANGE)
+      elsif df == "hard"
+        hoc_logic(HARD_RANGE)
+      end
+    end
+  end
+  rescue Interrupt
+    puts "\nQuitting..."
     exit
   end
-  
-  if gm == "classic"
-    puts "_________________________________________________"
-    print "|   Easy   |   "
-    print "Medium   |   "
-    print "Hard   |   "
-    print "Custom   |   "
-    puts ""
-    puts ""
-    print "CHOOSE THE DIFFICULTY: "
-    df = gets.chomp
-    df = df.downcase
-    puts ""
 
-    if !df_lvl.include?(df)
-      puts "Invalid difficulty."
-      exit
-    end
-
-    if df == "easy"
-      cl_logic(EASY_RANGE)
-    elsif df == "medium"
-      cl_logic(MID_RANGE)
-    elsif df == "hard"
-      cl_logic(HARD_RANGE)
-    elsif df == "custom"
-      cl_cs_logic
-    end
-  end
-
-  if gm == "binary"
-    bin_logic
-  end
-
-  if gm == "hoc"
-    puts "____________________________________"
-    print "|   Easy   |   "
-    print "Medium   |   "
-    print "Hard   |   "
-    puts ""
-    puts ""
-    print "CHOOSE THE DIFFICULTY: "
-    df = gets.chomp
-    df = df.downcase
-    puts ""
-
-    if !df_lvl.include?(df)
-      puts "Invalid difficulty."
-      exit
-    end
-
-    if df == "easy"
-      hoc_logic(EASY_RANGE)
-    elsif df == "medium"
-      hoc_logic(MID_RANGE)
-    elsif df == "hard"
-      hoc_logic(HARD_RANGE)
-    end
-  end
-end
-rescue Interrupt
-  puts "\nQuitting..."
-  exit
 end
 
+main if __FILE__ == $PROGRAM_NAME
